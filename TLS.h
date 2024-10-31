a136ce522236f57e523176985d380fdb0fa1e801
Denislav Ivanov <denislavivanov82@gmail.com>
2024-10-31 22:45:36 +0200

    Added TLS.h

---
+++ TLS.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+/* Initialize Library */
+BOOL TLS_Init(TLS* pTLS);
+BOOL TLS_Handshake(TLS* pTLS, SOCKET s, LPSTR szDomain);
+/* Send & Recv */
+int TLS_Send(TLS* pTLS, SOCKET s, PBYTE pBuff, int len);
+int TLS_Recv(TLS* pTLS, SOCKET s, PBYTE pBuff, int len);
+/* Cleanup */
+void TLS_Cleanup(TLS* pTLS);
